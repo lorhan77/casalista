625ee0feaf0dc485f7280647cdf3a8e04fe46c52
lorhan77 <gabriellorhan7@gmail.com>
2023-02-09 16:59:24 -0400

    0902

--- routes/presentes.js
+++ routes/presentes.js
@@ -2,6 +2,7 @@ module.exports = function (app) {
     var presentes = app.controllers.presentes;
     app.get("/presentes", presentes.index);
     app.get("/comprar/:id", presentes.comprar);
+    app.get("/apagar/:id", presentes.apagar);
     app.get("/listar", presentes.listar);
     app.post("/presentes", presentes.incluir);
     app.post("/incluirnobanco", presentes.incluirnobanco);
